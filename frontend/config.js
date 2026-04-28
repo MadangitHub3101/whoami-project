f332e151d73fe6cf53979534ab7938cd8b94837e
madanadatta <147394640+MadangitHub3101@users.noreply.github.com>
2026-04-28 13:15:19 +0530

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,6 @@
 // Frontend Configuration
 // UPDATE THIS with your Digital Ocean backend URL after deployment
 window.CONFIG = {
-  API_URL: 'https://whoami-project-production.up.railway.app'
+  API_URL: 'https://honest-enthusiasm-production.up.railway.app'
 };
  
